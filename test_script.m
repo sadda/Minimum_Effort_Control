4ebf5d89539ed2494fee5d5424f040fcf3552fa8
sadda <lukas.adam.cr@gmail.com>
2021-07-12 11:14:23 +0200

    Added get_a

--- test_script.m
+++ test_script.m
@@ -12,48 +12,8 @@ N = round(T/dt);
 Um = 1; %230*sqrt(2);
 
 A_type = 6;
+A = get_a(A_type);
 
-switch A_type
-    case 1
-        % 3-faz
-        A = 2/3*[1, cos(2/3*pi), cos(-2/3*pi);...
-            0, sin(2/3*pi), sin(-2/3*pi);];
-    case 2
-        % 5-faz 3 DOF
-        A = 2/5*[1, cos(2*pi/5), cos(4*pi/5), cos(-4*pi/5), cos(-2*pi/5); ...
-            0, sin(2*pi/5), sin(4*pi/5), sin(-4*pi/5), sin(-2*pi/5);];
-    case 3
-        % 5-faz 1 DOF
-        A = 2/5*[1, cos(2*pi/5), cos(4*pi/5), cos(-4*pi/5), cos(-2*pi/5); ...
-            0, sin(2*pi/5), sin(4*pi/5), sin(-4*pi/5), sin(-2*pi/5); ...
-            1, cos(4*pi/5), cos(-2*pi/5), cos(2*pi/5), cos(-4*pi/5); ...
-            0, sin(4*pi/5), sin(-2*pi/5), sin(2*pi/5), sin(-4*pi/5)];
-    case 4
-        % 7-faz 5 DOF
-        alpha = 2*pi/7;
-        A = 2/7*[1, cos(alpha), cos(2*alpha), cos(3*alpha), cos(4*alpha), cos(5*alpha), cos(6*alpha); ...
-            0, sin(alpha), sin(2*alpha), sin(3*alpha), sin(4*alpha), sin(5*alpha), sin(6*alpha);];
-    case 5
-        % 7-faz 1 DOF
-        alpha = 2*pi/7;
-        A = 2/7*[1, cos(alpha), cos(2*alpha), cos(3*alpha), cos(4*alpha), cos(5*alpha), cos(6*alpha);...
-            0, sin(alpha), sin(2*alpha), sin(3*alpha), sin(4*alpha), sin(5*alpha), sin(6*alpha);...
-            1, cos(2*alpha), cos(4*alpha), cos(6*alpha), cos(8*alpha), cos(10*alpha), cos(12*alpha);...
-            0, sin(2*alpha), sin(4*alpha), sin(6*alpha), sin(8*alpha), sin(10*alpha), sin(12*alpha);...
-            1, cos(3*alpha), cos(6*alpha), cos(9*alpha), cos(12*alpha), cos(15*alpha), cos(18*alpha);...
-            0, sin(3*alpha), sin(6*alpha), sin(9*alpha), sin(12*alpha), sin(15*alpha), sin(18*alpha);];
-    case 6
-        % 4-faz 1 DOF
-        A1 = 2/3*[1, cos(2/3*pi), cos(-2/3*pi);...
-            0, sin(2/3*pi), sin(-2/3*pi);...
-            1/2, 1/2, 1/2;];
-        
-        A2 = [1, 0, 0, -1;...
-            0, 1, 0, -1;...
-            0, 0, 1, -1;];
-        A = A1*A2;
-        
-end
 
 
 
