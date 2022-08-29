4d1b85f800fb5b3d07ed2b9c58deb203e3207bbc
sadda <lukas.adam.cr@gmail.com>
2022-08-29 15:41:37 +0200

    Plots improved

--- plot_figures.m
+++ plot_figures.m
@@ -3,35 +3,38 @@ function h = plot_figures(ts, xs, A1, A2, Um, y_lim, kappa)
     
     h = figure('visible', 'off', 'position', 4*[0, 0, 400, 100]);
     set(gcf, 'color', 'white')
-    axis tight manual % this ensures that getframe() returns a consistent size
+    axis tight manual% this ensures that getframe() returns a consistent size
     
-    subplot(1,4,1);
-    plot(ts, xs');
-    ylim(y_lim);
-    ylabel('kappa = ' + string(kappa))
-    title('x');
+    pos_x = 0.04;
+    width_x = 0.2;
+    offset_x = 0.05;
     
-    subplot(1,4,2);
-    plot(ts, (A1*xs)');
-    ylim(y_lim);
-    title('A*x');
-    
-    subplot(1,4,3);
-    hold on;
-    plot(ts, (A2([1 2],:)*xs)');
-    plot(ts, Um*kappa*ones(1,length(ts)), 'k--');
-    plot(ts, -Um*kappa*ones(1,length(ts)), 'k--');
-    ylim(y_lim);
-    title('B*x');
-    
-    subplot(1,4,4);
-    hold on;
-    plot((A1(1,:)*xs)', (A1(2,:)*xs)');
-    plot((A2(1,:)*xs)', (A2(2,:)*xs)');
-    plot(Um*kappa*cos(disc), Um*kappa*sin(disc), 'k--');
-    xlim(y_lim);
-    ylim(y_lim);
-    title('B*x');
-    
-    drawnow;
-end
+    for i = 1:4
+        subplot('Position', [pos_x+(i-1)*(width_x+offset_x) 0.08 width_x 0.86]);
+        if i == 1
+            plot(ts, xs');
+            ylim(y_lim);
+            ylabel('kappa = ' + string(kappa))
+            title('x');
+        elseif i == 2
+            plot(ts, (A1*xs)');
+            ylim(y_lim);
+            title('A*x');
+        elseif i == 3
+            plot(ts, (A2([1 2],:)*xs)');
+            hold on;
+            plot(ts, Um*kappa*ones(1,length(ts)), 'k--');
+            plot(ts, -Um*kappa*ones(1,length(ts)), 'k--');
+            ylim(y_lim);
+            title('B*x');
+        elseif i == 4
+            plot((A1(1,:)*xs)', (A1(2,:)*xs)');
+            hold on;
+            plot((A2(1,:)*xs)', (A2(2,:)*xs)');
+            plot(Um*kappa*cos(disc), Um*kappa*sin(disc), 'k--');
+            xlim(y_lim);
+            ylim(y_lim);
+            title('B*x');
+        end
+        drawnow;
+    end
