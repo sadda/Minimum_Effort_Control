3aa181afa254a0cf15cf219a2bc7dbcb943b3a2b
sadda <lukas.adam.cr@gmail.com>
2025-01-30 13:35:17 +0100

    Refactor

--- src/Pars.m
+++ src/Pars.m
@@ -154,3 +154,25 @@ function [multiples, multiples_counts] = find_column_multiples(A, tol)
     end
     multiples_counts = [multiples_unique, multiples_sum];
 end
+
+function [X_idx, idx] = linearly_independent_rows(X, tol)
+    if ~nnz(X)
+        error('Zero matrix');
+    end
+    if nargin < 2
+        tol=1e-10;
+    end
+
+    % QR decomposition (transposition for passing from columns to rows)
+    [~, R, E] = qr(X', 0);
+    if ~isvector(R)
+        diagr = abs(diag(R));
+    else
+        diagr = abs(R(1));
+    end
+
+    % Rank estimation
+    r = find(diagr >= tol*diagr(1), 1, 'last');
+    idx = sort(E(1:r));
+    X_idx = X(idx,:);
+end
